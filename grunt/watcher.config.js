7c524778ff720d9b71f7c9f2edbf577daf4b7813
Brice Argenson <bargenson@gmail.com>
2016-05-03 18:46:19 -0400

    Fix watcher paths

--- grunt/watcher.config.js
+++ grunt/watcher.config.js
@@ -2,14 +2,14 @@ module.exports = {
 
   watch: {
     css: {
-      files: ["src/css/*"],
+      files: ["src/css/**/*"],
       tasks: ["postcss:dev"],
       options: {
         spawn: false
       }
     },
     js: {
-      files: ["src/js/*"],
+      files: ["src/js/**/*"],
       tasks: ["webpack:dev"],
       options: {
         spawn: false
